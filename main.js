b3a7f861ac496aa2d8d7a0e50fe5254bee1b4a2a
dzhuleikhan <dzhuleikhan@gmail.com>
2023-12-25 16:18:25 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+import "./style.scss";
+
+import "./modules/lenis";
+import "./modules/slider";
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+export default {
+  server: {
+    host: true,
+    open: true,
+  },
+  buils: {},
+};
