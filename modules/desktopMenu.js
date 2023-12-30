c3dcf7117a876f0009efe09495d6a23463b94426
dzhuleikhan <dzhuleikhan@gmail.com>
2023-12-30 05:14:43 +0300

    1

--- modules/desktopMenu.js
+++ modules/desktopMenu.js
@@ -6,87 +6,60 @@ const overlay = document.querySelector(".overlay");
 const repairMenu = document.querySelector(".repair");
 const serviceMenu = document.querySelector(".service__menu");
 
-function openOverylay() {
-  document.getElementById("header").classList.add("is-white");
-  setTimeout(() => {
-    overlay.classList.add("is-open");
-  }, 400);
-}
-function closeOverylay() {
-  overlay.classList.remove("is-open");
-  setTimeout(() => {
-    document.getElementById("header").classList.remove("is-white");
-  }, 400);
-}
-
-function closeAllMenus() {
-  closeOverylay();
-  closeRepairMenu();
-  closeServiceMenu();
-}
-
-function openRepairMenu() {
-  repairMenu.classList.add("is-open");
-}
-function closeRepairMenu() {
-  repairMenu.classList.remove("is-open");
-}
-function openServiceMenu() {
-  serviceMenu.classList.add("is-open");
-}
-function closeServiceMenu() {
-  serviceMenu.classList.remove("is-open");
-}
-
+// Repair
 repairLink.addEventListener("mouseover", (e) => {
-  if (!repairMenu.classList.contains("is-open")) {
+  if (repairLink.classList.contains("is-open")) {
+    return false;
+  } else {
     document.getElementById("header").classList.add("is-white");
     setTimeout(() => {
-      openOverylay();
-      closeServiceMenu();
-      openRepairMenu();
+      overlay.classList.add("is-open");
+      serviceMenu.classList.add("is-open");
+      repairMenu.classList.add("is-open");
     }, 200);
     lenis.destroy();
     document.querySelector("body").style.overflow = "hidden";
-  } else {
-    closeAllMenus();
-    document.querySelector("body").style.overflow = "visible";
   }
 });
 repairMenu.addEventListener("mouseleave", () => {
-  setTimeout(() => {
-    document.getElementById("header").classList.remove("is-white");
-  }, 200);
-  closeAllMenus();
-  document.querySelector("body").style.overflow = "visible";
-});
-serviceLink.addEventListener("mouseover", (e) => {
-  e.preventDefault();
-  if (!serviceMenu.classList.contains("is-open")) {
-    document.getElementById("header").classList.add("is-white");
-    setTimeout(() => {
-      openOverylay();
-      closeRepairMenu();
-      openServiceMenu();
-    }, 200);
-    lenis.destroy();
-    document.querySelector("body").style.overflow = "hidden";
+  if (serviceLink.classList.contains("is-opem")) {
+    return false;
   } else {
-    closeAllMenus();
+    repairMenu.classList.remove("is-open");
+    overlay.classList.remove("is-open");
+    serviceMenu.classList.remove("is-open");
     document.querySelector("body").style.overflow = "visible";
+    setTimeout(() => {
+      document.getElementById("header").classList.remove("is-white");
+    }, 400);
   }
+});
+// Service
+serviceLink.addEventListener("mouseover", (e) => {
+  document.getElementById("header").classList.add("is-white");
+  setTimeout(() => {
+    overlay.classList.add("is-open");
+    repairMenu.classList.remove("is-open");
+    serviceMenu.classList.add("is-open");
+  }, 200);
+  lenis.destroy();
+  document.querySelector("body").style.overflow = "hidden";
 });
 serviceMenu.addEventListener("mouseleave", () => {
   setTimeout(() => {
     document.getElementById("header").classList.remove("is-white");
   }, 200);
-  closeAllMenus();
-  lenis.start();
+  repairMenu.classList.remove("is-open");
+  overlay.classList.remove("is-open");
+  serviceMenu.classList.remove("is-open");
+  document.querySelector("body").style.overflow = "visible";
 });
 
 overlay.addEventListener("click", (e) => {
   if (e.target === overlay) {
-    closeAllMenus();
-    lenis.start();
+    repairMenu.classList.remove("is-open");
+    overlay.classList.remove("is-open");
+    serviceMenu.classList.remove("is-open");
+    document.querySelector("body").style.overflow = "visible";
   }
 });
